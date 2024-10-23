5cb525eb0f23d0c1592746918aae333116e9f4c4
ali <sa.ahmadnejad@gmail.com>
2024-10-23 11:04:21 +0330

    run sample agent

--- src/main/java/ir/donbee/mas/jade/App.java
+++ src/main/java/ir/donbee/mas/jade/App.java
@@ -1,10 +1,25 @@
 package ir.donbee.mas.jade;
 
-/**
- * Hello world!
- */
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Runtime instance = Runtime.instance();
+        ProfileImpl profile = new ProfileImpl();
+        profile.setParameter(Profile.GUI, "true");
+        AgentContainer mainContainer = instance.createMainContainer(profile);
+        AgentController sampleAgent = null;
+        try {
+            sampleAgent = mainContainer
+                    .createNewAgent("sampleAgent", "ir.donbee.mas.jade.agents.SampleAgent", null);
+            sampleAgent.start();
+        } catch (StaleProxyException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/ir/donbee/mas/jade/agents/SampleAgent.java
@@ -0,0 +1,13 @@
+package ir.donbee.mas.jade.agents;
+
+
+import jade.core.Agent;
+
+public class SampleAgent extends Agent {
+
+    @Override
+    public void setup() {
+        super.setup();
+        System.out.println("SampleAgent setup!");
+    }
+}
